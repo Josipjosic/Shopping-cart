d544923aeba0f13545e1a2dbc5b3fb60e45521dd
Josip Josic <71074064+Josipjosic@users.noreply.github.com>
2021-09-04 12:00:29 +0200

    Fixed scroll
    Added new JS code to make cart scroll

--- js/Function.js
+++ js/Function.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
-    const item_width = $('#divItemContent').outerWidth();
-
+    const item_height = $('#divItemContent').innerHeight();
+    const item_move = 133.81  // defines height of the item ( moze se ici i na = item_height, ali zbog margina i dividera ne bude cisti pomak te se vidi vise od 3 proizvoda pri scroll )
 
     $('.shoppingImg').on("click",()=>{
         $('#divCartContent').toggle();
     });
 
-    $( ".divButtonDOWN" ).on("click",() => {
-        const moveItemHeight = parseInt($('#divItemContent')) + item_width
-        $( "#divItemContent" ).animate({ "margin-bottom": moveItemHeight }, "slow" );
+    $( ".divButtonDOWN" ).on("click",() => {     // on click function on button
+        $( "#divItemContentMain" ).animate({       //animates selected div
+            scrollTop: $('#divItemContentMain').scrollTop() + item_move  // scrolls items by their height 
+        });
       });
 
       $( ".divButtonUP" ).on("click",() => {
-        const moveItemHeight = parseInt($('#divItemContent')) + item_width
-        $( "#divItemContent" ).animate({ "margin-top": moveItemHeight }, "slow" );
+        $( "#divItemContentMain" ).animate({
+            scrollTop: $('#divItemContentMain').scrollTop() - item_move
+            }, "slow" );
       });
 });
